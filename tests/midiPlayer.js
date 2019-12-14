9dccc2aa37efe6ebd0c4fcad79798f203bc36e4d
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-12-14 09:50:37 -0800

    Update midiPlayer.js

--- tests/midiPlayer.js
+++ tests/midiPlayer.js
@@ -24,7 +24,7 @@ describe('MidiPlayer', function() {
             expect(midiPlayer.logging).toBe(false);
         });
 
-        test('Player should instantiate with logging turned ON', function() {
+        test('Player should instantiate with console logging turned ON', function() {
             midiPlayer = new MidiPlayer({ logging: true });
             expect(midiPlayer).toBeInstanceOf(MidiPlayer);
             expect(midiPlayer.eventLogger).toBeUndefined();
