3cfafc72817d63690cfb238605431a1edfaab87c
yvesgurcan <gurcan.yves+github@gmail.com>
2019-12-21 23:58:13 -0800

    jest cfg

--- jest.config.js
+++ jest.config.js
@@ -115,7 +115,7 @@ module.exports = {
     // rootDir: null,
 
     // A list of paths to directories that Jest should use to search for files in
-    roots: ['<rootDir>'],
+    roots: ['<rootDir>/src', '<rootDir>/tests/'],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
