beee89b444e8aaf0ed6913a34a3fec24641347ca
yvesgurcan <gurcan.yves+github@gmail.com>
2019-12-22 00:15:31 -0800

    remove use of clean webpack plugin

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,15 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+console.log(CleanWebpackPlugin);
+
 module.exports = {
     output: {
         filename: '[name].[hash].js',
         path: path.resolve(__dirname, './')
     },
     plugins: [
-        new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['main.*.js']
-        }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
