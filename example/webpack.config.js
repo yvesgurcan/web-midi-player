62de5cff63fac3185e2deecaf8e98ae3fdf20e86
yvesgurcan <gurcan.yves+github@gmail.com>
2019-12-22 00:19:04 -0800

    remove styled component babel plugin

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-console.log(CleanWebpackPlugin);
-
 module.exports = {
     output: {
         filename: '[name].[hash].js',
