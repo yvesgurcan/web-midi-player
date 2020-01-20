35ce660c28aefa59d4cff70512f36f5d084351bc
Yves Gurcan <yves.gurcan@nike.com>
2020-01-19 22:18:56 -0800

    only initialize audio context if defined

--- src/MidiPlayer.js
+++ src/MidiPlayer.js
@@ -30,7 +30,7 @@ export default class MidiPlayer {
      * @param {function} [configuration.eventLogger = undefined] The function that receives event payloads.
      * @param {boolean} [configuration.logging = false] Turns ON or OFF logging to the console.
      * @param {string} [configuration.patchUrl = https://cdn.jsdelivr.net/npm/midi-instrument-patches@latest/] The public path where MIDI instrument patches can be found.
-     * @param {object} [configuration.audioContext] An instance of the Web Audio API AudioContext interface.
+     * @param {object} [configuration.audioContext = undefined] An instance of the Web Audio API AudioContext interface.
      * @property {string} playerId ID of this instance of Midi Player.
      * @property {object} context The AudioContext instance.
      * @property {function} eventLogger The function that is called to emit events.
@@ -77,13 +77,19 @@ export default class MidiPlayer {
             this.eventLogger = eventLogger;
             this.logging = logging;
             this.patchUrl = patchUrl;
-            this.context = audioContext || new AudioContext();
             this.startTime = 0;
+
+            if (audioContext) {
+                this.context = audioContext;
+            }
+
             LibTiMidity.init(isFirstInstance);
+
             this.isFirstInstance = isFirstInstance;
             if (isFirstInstance) {
                 isFirstInstance = false;
             }
+
             this.eventHandler.emitInit();
         } catch (error) {
             this.emitEvent({
