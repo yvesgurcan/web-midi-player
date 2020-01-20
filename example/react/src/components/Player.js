3f65c4f538e3847cd871d2dc203aa091c5a2da5e
Yves Gurcan <yves.gurcan@nike.com>
2020-01-19 22:20:45 -0800

    unused import

--- example/react/src/components/Player.js
+++ example/react/src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import MidiPlayer from 'web-midi-player';
 
