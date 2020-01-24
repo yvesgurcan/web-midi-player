d8d7fda6a735198cb018e2dc6282c6759a38679f
Yves Gurcan <gurcan.yves+github@gmail.com>
2020-01-24 00:21:31 -0800

    script

---
+++ scripts/gpr.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const { join } = require('path')
+
+// Get the package obejct and change the name
+const pkg = require('../package.json')
+pkg.name = '@yvesgurcan/web-midi-player'
+
+// Update package.json with the udpated name
+fs.writeFileSync(join(__dirname, '../package.json'), JSON.stringify(pkg))
